243faf2aa3f5cfe07a0ff1ba3057ac529491041a
Alex Redington, Sam Umbach and Russ Olsen <pair@thinkrelevance.com>
2011-12-09 16:58:30 -0500

    Tweaks to add an nginx site. Very brittle right now.

--- site-cookbooks/rails_app/recipes/default.rb
+++ site-cookbooks/rails_app/recipes/default.rb
@@ -10,15 +10,62 @@
 user "deploy" do
   shell "/bin/bash"
   action :create
+  group 'deploy'
+end
+
+group 'admin' do
+  append true
+  members ['deploy']
+end
+
+directory "/home/deploy" do
+  owner 'deploy'
+  group 'deploy'
+  mode 0755
 end
 
 directory "/home/deploy/.ssh" do
-  mode 0600
-  recursive true
+  owner 'deploy'
+  group 'deploy'
+  mode 0755
 end
 
 file "/home/deploy/.ssh/authorized_keys" do
   all_authorized_keys = Dir[File.expand_path("../../files/default/public_keys/*.pub",__FILE__)]
   content all_authorized_keys.map {|path| File.read(path)}.join
+  owner 'deploy'
+  group 'deploy'
   mode 0600
+end
+
+directory "/var/www/apps/" do
+  recursive true
+  owner 'deploy'
+  group 'deploy'
+  mode 02775
+end
+
+file "/home/deploy/.bashrc" do
+  content "export PATH=#{node[:ruby_enterprise][:install_path]}/bin:$PATH"
+  owner 'deploy'
+  group 'deploy'
+  mode 0700
+end
+
+file "/home/deploy/.bash_profile" do
+  content 'source "$HOME/.bashrc"'
+  owner 'deploy'
+  group 'deploy'
+  mode 0700
+end
+
+template "#{node[:nginx][:dir]}/sites-available/elzar_test" do
+  source "rails_app_nginx.erb"
+  mode 0440
+  owner "root"
+  group "root"
+end
+
+nginx_site "elzar_test" do
+  enable true
 end
