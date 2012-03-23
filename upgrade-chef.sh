c4889acb322dbb01508b9f5945b167890a3d7e76
Alex Redington <pair@thinkrelevance.com>
2012-03-23 11:07:23 -0400

    Remove pubkeys, make upgrade-chef.sh work

--- upgrade-chef.sh
+++ upgrade-chef.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-if `gem list -d chef | grep "10.2"`
+gem list -d chef | grep "0.10.2"
+OLD_CHEF_INSTALLED=$?
+if [ $OLD_CHEF_INSTALLED -eq 0 ]
 then
-    gem install chef -v '=0.10.8' --no-ri --no-rdoc && gem uninstall chef -v '<0.10.8'
+    gem install chef -v '=0.10.8' --no-ri --no-rdoc
+    gem uninstall chef -v '<0.10.8'
 fi
