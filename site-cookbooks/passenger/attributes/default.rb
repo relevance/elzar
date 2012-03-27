d473d38ab8883b9abc295928480badca30ebdf22
Gabriel Horner <gabriel.horner@gmail.com>
2012-03-27 17:09:50 -0400

    #3: add passenger attributes

---
+++ site-cookbooks/passenger/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:passenger][:version] = '3.0.11'
+default[:root_path] = "/opt/ruby/lib/ruby/gems/1.8/gems/passenger-3.0.11",
+default[:module_path] =  "/opt/ruby/lib/ruby/gems/1.8/gems/passenger-3.0.11/ext/apache2/mod_passenger.so"
