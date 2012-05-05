0223c80bb1a46c71720853bb8f01f8d4d4c198a0
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-05 00:04:03 -0400

    create local provison dir

--- lib/elzar.rb
+++ lib/elzar.rb
@@ -3,30 +3,54 @@ require 'elzar/template'
 require 'fileutils'
 
 module Elzar
+  def self.root_dir
+    @root_dir ||= File.expand_path File.dirname(__FILE__) + '/../'
+  end
+
+  def self.templates_dir
+    @templates_dir ||= "#{root_dir}/lib/elzar/templates"
+  end
+
+  def self.provision_dir
+    "#{root_dir}/provision"
+  end
+
   def self.bam!(options={})
-    options = {:destination => 'provision', :authorized_keys => []}.update options
-    FileUtils.mkdir_p options[:destination]
-    Template.destination_directory = options[:destination]
-    root_dir = File.expand_path File.dirname(__FILE__) + '/../'
-    templates_dir = "#{root_dir}/lib/elzar/templates"
+    options = {:destination => 'provision'}.update options
+    # Template.destination_directory = options[:destination]
+    create_local_provision_dir provision_dir
+    create_remote_provision_dir options[:destination]
+  end
 
-    cp "#{templates_dir}/solo.rb", options[:destination]
-    cp "#{templates_dir}/dna.json", options[:destination]
-    cp "#{templates_dir}/Gemfile", options[:destination]
-    cp "#{templates_dir}/Vagrantfile", options[:destination]
-    cp_r "#{root_dir}/data_bags", options[:destination]
-    cp_r "#{root_dir}/cookbooks", options[:destination]
-    cp_r "#{root_dir}/site-cookbooks", options[:destination]
-    cp "#{root_dir}/.rvmrc", options[:destination]
+  private
 
+  def self.create_local_provision_dir(dest)
+    FileUtils.rm_rf dest
+    FileUtils.mkdir_p "#{dest}/elzar"
+
+    cp "#{templates_dir}/solo.rb", dest
+    cp_r "#{root_dir}/data_bags", dest
+    cp_r "#{root_dir}/roles", dest
+    cp_r "#{root_dir}/cookbooks", "#{dest}/elzar"
+    cp_r "#{root_dir}/site-cookbooks", "#{dest}/elzar"
+  end
+
+  def self.create_remote_provision_dir(dest)
+    FileUtils.mkdir_p dest
+    cp "#{templates_dir}/dna.json", dest
+    cp "#{templates_dir}/Gemfile", dest
+    cp "#{templates_dir}/Vagrantfile", dest
+    cp "#{root_dir}/.rvmrc", dest
+    cp_r "#{root_dir}/script", dest
+  end
+
+  def self.generate_files
     # TODO
     # generate_vagrantfile(options[:app_name])
     # :ruby, :database, :gem_version
     # generate_dna_json(options)
     # generate_data_bags
   end
-
-  private
 
   def self.cp(*args)
     FileUtils.cp(*args)
