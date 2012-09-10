e2946c9f1757ccf9a2d446659053afb563df0369
Jared Pace and Jason Rudolph <pair+jp+jr@thinkrelevance.com>
2012-09-10 15:17:20 -0400

    WIP Rename command and options based on LJK chat

--- lib/elzar/cli.rb
+++ lib/elzar/cli.rb
@@ -4,6 +4,7 @@ module Elzar
   module Cli
     def self.install(options, args)
       Elzar.create_provision_directory 'provision', options
+      puts 'Created /provision directory'
     end
   end
 end
